fixed normalization function

sonoUno/data_transform/predef_math_functions.py
@@ -47,9 +47,12 @@ class PredefMathFunctions(object):
                 new_y.fill(0)
             else:
                 #Perform the normalization
+                new_x = ((data_x-np.nanmin(data_x)) 
+                    / (np.nanmax(data_x)-np.nanmin(data_x)))
+
                 new_y = ((data_y-np.nanmin(data_y)) 
                     / (np.nanmax(data_y)-np.nanmin(data_y)))
-            return data_x, new_y, True
+            return new_x, new_y, True
         except Exception as Error:
             self._export_error_info.writeexception(Error)
             return data_x, data_y, False